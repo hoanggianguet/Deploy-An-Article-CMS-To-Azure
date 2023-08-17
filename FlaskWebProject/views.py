ccd4f4339f57efb818d8c9abdac6946d36032b56
hoanggianguet <gianghoang.uet.vnu@gmail.com>
2023-08-17 19:32:56 +0700

    Update conffig
    Update config

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -13,7 +13,9 @@ from FlaskWebProject.models import User, Post
 import msal
 import uuid
 
-imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER']  + '/'
+imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + \
+    '.blob.core.windows.net/' + app.config['BLOB_CONTAINER'] + '/'
+
 
 @app.route('/')
 @app.route('/home')
@@ -27,13 +29,15 @@ def home():
         posts=posts
     )
 
+
 @app.route('/new_post', methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm(request.form)
     if form.validate_on_submit():
         post = Post()
-        post.save_changes(form, request.files['image_path'], current_user.id, new=True)
+        post.save_changes(
+            form, request.files['image_path'], current_user.id, new=True)
         return redirect(url_for('home'))
     return render_template(
         'post.html',
@@ -58,6 +62,7 @@ def post(id):
         form=form
     )
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -66,7 +71,8 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.warning("Logging in failed for user:{}".format(form.username.data))
+            app.logger.warning(
+                "Logging in failed for user:{}".format(form.username.data))
             flash('Invalid username or password')
             return redirect(url_for('login'))
         app.logger.info("Logging in user:{}".format(form.username.data))
@@ -79,7 +85,9 @@ def login():
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
-@app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
+
+# Its absolute URL must match your app's redirect_uri set in AAD
+@app.route(Config.REDIRECT_PATH)
 def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
@@ -92,7 +100,7 @@ def authorized():
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
-            redirect_uri=url_for('authorized', _external=True, _scheme='https'))  
+            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -104,10 +112,11 @@ def authorized():
         app.logger.info("You are logged in successfully")
     return redirect(url_for('home'))
 
+
 @app.route('/logout')
 def logout():
     logout_user()
-    if session.get("user"): # Used MS Login
+    if session.get("user"):  # Used MS Login
         # Wipe out user and its token cache from session
         session.clear()
         # Also logout from your tenant's web session
@@ -117,12 +126,14 @@ def logout():
 
     return redirect(url_for('login'))
 
+
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists:
     cache = msal.SerializableTokenCache()
     if session.get("token_cache"):
         cache.deserialize(session["token_cache"])
     return cache
+
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
@@ -135,6 +146,7 @@ def _build_msal_app(cache=None, authority=None):
     return msal.ConfidentialClientApplication(
         Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
         client_credential=Config.CLIENT_SECRET, token_cache=cache)
+
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
--- config.py
+++ config.py
@@ -2,24 +2,29 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsstoragedb'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'pedubXBRo5EeVDwDqA2FMT3dRGUedrIuyXgy+Ei0A87CJ0PhLfIbSBlWO5aC2CrXwWcl8ZrwCCf1+AStXDXzZQ=='
+    BLOB_STORAGE_KEY = os.environ.get(
+        'BLOB_STORAGE_KEY') or 'ycEcVnhijJ7J7kVwfcLywaQc8Gs7hyAlY6Q15CW7aEH8G3wd6hDEVpqMsQ58EgYkJSIQMo4aTIf2+AStOUOlww=='
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'cms-container'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms-database-server.database.windows.net'
+    SQL_SERVER = os.environ.get(
+        'SQL_SERVER') or 'cms-database-server.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'udacityadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'UdacityPassword123'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + \
+        SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + \
+        SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "PxY8Q~CTgHAGCrdUBzZOq93jv5-hiwKDT2gqHbE1"
+    CLIENT_SECRET = "3h78Q~8W4.wrvogfWI0zeVQgqulEMiKpbaHvxaFA"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -27,15 +32,17 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/common"
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "89d2a291-53d5-49f5-833c-e6035e9a4202"
+    CLIENT_ID = "4b6f697a-02ce-42ee-b11d-f6f033265e06"
 
-    REDIRECT_PATH = "https://cms-webapp-hg.azurewebsites.net/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/getAToken"
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
+    SCOPE = ["User.Read"]  # Only need to read user profile for this app
 
     SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
